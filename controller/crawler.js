19ea07d1f9e9775704cd32cbc422254d2d0cb9fc
arun-cc <arun.elangovan@cognitiveclouds.com>
2019-05-25 17:14:56 +0530

    crawl made recursive

--- controller/crawler.js
+++ controller/crawler.js
@@ -1,52 +1,56 @@
 'use strict';
 
 const uuid = require('react-native-uuid');
-const Crawler = require("crawler");
+const request = require('request-promise');
+const HTML = require('html-parse-stringify')
+const striptags = require('striptags');
+
 const _ = require("lodash");
 
 const CONFIG = require('../config');
 
 const Cawler_Db = require('../models/crawler');
 
-exports.get = (req, res) => {
-    
-    let c = new Crawler({
-        maxConnections : 5,
-        // This will be called for each crawled page
-        callback : function (error, response, done) {
-            if(error){
-                console.log(error);
-                res.status(error.status || 400).send("CRWALING FAILED");
-                return;
-            }else{
-                let $ = response.$;
-                let urls = $("a");
-                let href = [];
-                let final_href_arr = [];
-                Object.keys(urls).forEach((item) => {
-                    if (urls[item].type === 'tag') {
-                        href.push({'_id': uuid.v4(), 'url': urls[item].attribs.href});
+global.itemInProcessCache = [];
+global.items = []; 
+
+exports.get = () => {
+    setTimeout(function () {
+        exports.get();
+    }, 500)
+    console.log(global.itemInProcessCache);
+    if (global.itemInProcessCache.length < 5) {
+        global.itemInProcessCache.push(global.itemInProcessCache.length + 1);
+        const options = {
+            method: 'GET',
+            url: CONFIG.env_variables.crawl_url
+        };
+        request(options).then(body => {
+            let href = [];
+            let final_href_arr = [];
+            let a = striptags(body, ['a']);
+            let html = HTML.parse(a);
+            Object.keys(html).forEach((item) => {
+                if (html[item].type === 'tag') {
+                    href.push({'_id': uuid.v4(), 'url': html[item].attrs.href});
+                }
+            });
+            let unique_hrefs = _.uniqBy(href, 'url');
+            unique_hrefs.forEach(e => {
+                let obj = e;
+                let count = 0;
+                href.forEach(i => {
+                    if (e.url.split('?')[0] === i.url.split('?')[0]) {
+                        count = count + 1;
                     }
-                });
-                let unique_hrefs = _.uniqBy(href, 'url');
-                unique_hrefs.forEach(e => {
-                    let obj = e;
-                    let count = 0;
-                    href.forEach(i => {
-                        if (e.url.split('?')[0] === i.url.split('?')[0]) {
-                            count = count + 1;
-                        }
-                    })
-                    obj['reference_count'] = count;
-                    obj['params'] = e.url.split('?')[1];
-                    final_href_arr.push(obj);
                 })
-                Cawler_Db.bulkCreate(final_href_arr);
-                res.status(200).send('CRAWLING DONE');
-            }
-            done();
-        }
-    });
-    c.queue(CONFIG.env_variables.crawl_url);
+                obj['reference_count'] = count;
+                obj['params'] = e.url.split('?')[1];
+                final_href_arr.push(obj);
+            })
+            Cawler_Db.bulkCreate(final_href_arr);
+            global.itemInProcessCache.pop();
+        });
+    }
 }
 
--- db_connection/db.js
+++ db_connection/db.js
@@ -7,7 +7,7 @@ const DB_CONFIG = require('../config');
 const connection = new Sequelize(DB_CONFIG.env_variables.db_name,  DB_CONFIG.env_variables.db_user, DB_CONFIG.env_variables.db_password, {
   host: DB_CONFIG.env_variables.db_host,
   dialect: 'mysql',
-  //logging:false
+  logging:false
 });
 
 module.exports = {
